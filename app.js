9283519a2b2b1baa3bcab42d70be11144ef35b45
rothersar <alex.xd.2011@yandex.ru>
2021-03-24 14:48:56 +0300

    try redirect again

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.get("/callback", function (req, res) {
         console.log(result);
         if (result.access_token) {
           res.send(`${result.access_token},${result.refresh_token}`);
-          return "1234";
+          return res.redirect("https://github.com/");
         } else {
           res.redirect(
             "/#" +
