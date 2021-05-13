cac206d0f6c5ac2007f001722d42fb15567726f6
rothersar <alex.xd.2011@yandex.ru>
2021-05-13 15:05:20 +0300

    test

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ app.get("/callback", function (req, res) {
             //   `https://proyecto26.github.io/react-native-inappbrowser?redirect_url=${redirUri}`
             // );
             .redirect(
-              `https://github.com/?access_token=${result.access_token}&refresh_token=${result.refresh_token}`
+              `http//www.app_name.com?access_token=${result.access_token}&refresh_token=${result.refresh_token}`
             );
         } else {
           res.redirect(
